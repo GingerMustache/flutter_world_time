5f69b4f81f64215da9969e04d3935a3d1ce7280c
Ginger_mustache <artzaneg@gmail.com>
2023-06-13 13:51:52 +0300

    add simulator off using data to check async work

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
@@ -8,17 +9,49 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  void getData() async {
+    // simulate network request for username
+    String username = await Future.delayed(const Duration(seconds: 3), () {
+      return "username";
+    });
+
+    // simulate network request for bio of username
+    String bio = await Future.delayed(const Duration(seconds: 2), () {
+      return "good man, but still not a woman";
+    });
+
+    print("username - $username\nbio - $bio");
+  }
+
+  @override
+  // всегда отрабатывает вперед build
+  void initState() {
+    super.initState();
+    getData();
+    print("hey, there");
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("build function run");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text("choose a location"),
+        title: const Text("choose a location"),
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Text("choos location screen"),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text(counter.toString()),
+      ),
     );
   }
 }
