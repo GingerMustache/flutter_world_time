fe4d5416cb0d17ba11927174343aa77ef8c37177
Ginger_mustache <artzaneg@gmail.com>
2023-06-15 10:50:33 +0300

    little play with images and fonts

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,42 +17,49 @@ class _HomeState extends State<Home> {
     print(data);
 
     // set a background
-    // String bgImage = data["isDaytime"] ? "day.jpg" : "night.jpg";
+    String bgImage = data["isDayTime"] ? "day.jpg" : "night.jpg";
 
     return Scaffold(
       body: SafeArea(
           child: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("images/night.jpg"),
+            image: AssetImage("images/$bgImage"),
             fit: BoxFit.cover,
           ),
         ),
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(0.0, 120, 0.0, 0.0),
+          padding: const EdgeInsets.fromLTRB(0.0, 345, 0.0, 0.0),
           child: Column(
             children: [
               TextButton.icon(
                 onPressed: () {
                   Navigator.pushNamed(context, '/location');
                 },
-                icon: const Icon(Icons.edit_location),
-                label: const Text("Edit location"),
+                icon: const Icon(
+                  Icons.edit_location,
+                ),
+                label: Text(
+                  "Edit location",
+                ),
               ),
-              const SizedBox(height: 20.0),
+              const SizedBox(height: 8.0),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     data["location"],
-                    style: const TextStyle(fontSize: 28.0, letterSpacing: 2.0),
+                    style: const TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white),
                   )
                 ],
               ),
-              const SizedBox(height: 20.0),
+              const SizedBox(height: 15.0),
               Text(
                 data["time"],
-                style: const TextStyle(fontSize: 66.0),
+                style: const TextStyle(fontSize: 50.0, color: Colors.white),
               ),
             ],
           ),
